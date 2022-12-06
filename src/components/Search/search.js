cb5aa70255122282bf1fac88c795249c41ddebb3
Ramin <Raminshakooei@gmail.com>
2022-12-06 10:55:59 +0330

    theme color deleted search animation

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import TaskCtx from "../../store/task-context";
 import classes from "./Search.module.css";
 const Search = () => {
@@ -7,6 +7,17 @@ const Search = () => {
     ctx.search(e.target.value);
   };
 
+  const [isVisible, setIsVisible] = useState(false);
+  const SearchBoxClass = `${classes["search-input"]} ${
+    isVisible ? "" : classes["hide"]
+  }`;
+  const SearchIconClass = `${classes["search-icon"]} ${
+    isVisible ? classes["move-left"] : ""
+  }`;
+
+  const clickSearchHandler = () => {
+    setIsVisible((prev) => !prev);
+  };
   return (
     <div className={classes["search-box"]}>
       <svg
@@ -14,13 +25,14 @@ const Search = () => {
         width="24"
         height="24"
         viewBox="0 0 24 24"
-        className={classes["search-icon"]}
+        className={SearchIconClass}
+        onClick={clickSearchHandler}
       >
         <path d="M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z" />
       </svg>
       <input
         type="search"
-        className={classes["search-input"]}
+        className={SearchBoxClass}
         onChange={searchHandler}
       ></input>
     </div>
